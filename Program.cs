efa3ed439e8dbd8fb1cd2529ea2e05d6ffa42067
Jesika Raut <jsscraut@gmail.com>
2025-11-05 09:42:49 +0545

    first commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+string userName = "jessica";
+int luckyNumber = 12;
+
+//Console.WriteLine("Hello, " + userName + "!");
+//Console.WriteLine("Lucky Number, " + luckyNumber + "!");
+
+Console.WriteLine($"Hello, {userName}! Your lucky number is {luckyNumber}.");
