686f12d2633eb48073619e002c49857fb6961b61
Jesika Raut <jsscraut@gmail.com>
2025-11-05 11:17:30 +0545

    Task-4 code

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+
+int[] favoriteNumbers = { 7, 3, 9, 1, 5 };
+
+
+Array.Sort(favoriteNumbers);
+
+
+Array.Reverse(favoriteNumbers);
+
+
+Console.WriteLine("Array elements:");
+for (int i = 0; i < favoriteNumbers.Length; i++)
+{
+    Console.WriteLine(favoriteNumbers[i]);
+}
+
+
+int searchNumber = 7;
+int position = Array.IndexOf(favoriteNumbers, searchNumber);
+Console.WriteLine($"\nNumber {searchNumber} is at index: {position}");
