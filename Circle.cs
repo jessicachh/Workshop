4ec7ad09cabaa8f70b21928afacf5f1344a8fc81
Jesika Raut <jsscraut@gmail.com>
2025-11-05 09:59:38 +0545

    first commit

---
+++ Circle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public class Circle
+    {
+        public const double PI = 3.14;
+
+        public double FindArea(double radius)
+        {
+            return PI * radius * radius;
+        }
+
+        public double FindPerimeter(double radius)
+        {
+            return 2 * PI * radius;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using Task2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Try to change the constant value (uncomment next line to see error)
+        // Circle.PI = 3.14159;   // ❌ Error: You cannot change a constant value
+
+        // Explanation:
+        // PI is a constant, so its value cannot be changed once given.
+
+        Circle c = new Circle();
+
+        double radius = 5;
+
+        Console.WriteLine("Area of circle: " + c.FindArea(radius));
+        Console.WriteLine("Perimeter of circle: " + c.FindPerimeter(radius));
+    }
+}
